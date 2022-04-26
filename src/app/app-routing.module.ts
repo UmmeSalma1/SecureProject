95cab434dc9d292033d7d8c0d6fa8cda4c3f6031
Umme Salma <ummesalma2298@gmail.com>
2022-04-26 12:09:40 +0530

    setup the url in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: HomeComponent },
+  { path: '/aboutus', component: AboutusComponent },
+  { path: '/contact', component: ContactusComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
