90ca21e8c6ccc4d032ac554cee659f783b20250d
Umme Salma <ummesalma2298@gmail.com>
2022-05-04 14:43:19 +0530

    call admin api in service

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  baseurl:string= "http://127.0.0.1:8001";
+
+  constructor(private http: HttpClient) { }
+
+  adminDetails(): Observable<any> {
+    return this.http.get<any>(this.baseurl+'/api/admin');
+  }
+
 }
