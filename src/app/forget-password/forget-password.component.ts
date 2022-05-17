e3f8ea8b9d25eaaf1a4c90ced9b8557254353918
Umme Salma <ummesalma2298@gmail.com>
2022-05-18 00:28:06 +0530

    forget-password animation

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ApiService } from '../shared/api.service';
+import { NgToastService } from 'ng-angular-popup';
 import { AuthService } from '../shared/auth.service';
 
 @Component({
@@ -11,14 +12,13 @@ import { AuthService } from '../shared/auth.service';
   styleUrls: ['./forget-password.component.css']
 })
 export class ForgetPasswordComponent implements OnInit {
-  forgetPasswordForm !: FormGroup;
-  errors: any = null;
-
+forgetPasswordForm !: FormGroup;
+errors: any = null;
 
   constructor(public router: Router,
     private formBuilder: FormBuilder,
     private api: ApiService,
-    private auth: AuthService) { }
+    private toast:NgToastService, private auth: AuthService) { }
 
   ngOnInit(): void {
     this.forgetPasswordForm = this.formBuilder.group({
@@ -31,14 +31,18 @@ export class ForgetPasswordComponent implements OnInit {
     console.log(this.forgetPasswordForm.value);
     this.auth.sendEmail(this.forgetPasswordForm.value).subscribe(
       (result) => {
-        console.log(result);
-        alert('Password reset link has been sent Successfully');
+        // console.log(result);
+        // alert('Password reset link has been sent Successfully');
         this.forgetPasswordForm.reset();
         // this.dialogRef.close("Sign Up");
-    this.router.navigate(['forgetpopup']);
+    // this.router.navigate(['forgetpopup']);
+    this.toast.success({detail:"Success Message",summary:" Please Check Your Mail, We Send a Link !!",duration:5000})
+    this.router.navigate(['/forgetpopup']);
   },
   (error) => {
     this.errors = error.error;
+    this.toast.error({detail:"Failed Message",summary:"Something went wrong, Try again!!",duration:5000})
+
   },
   );
 }
