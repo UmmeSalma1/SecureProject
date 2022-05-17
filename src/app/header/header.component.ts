2cb8f99578e06642096374a761ae58f432050766
Umme Salma <ummesalma2298@gmail.com>
2022-05-18 00:30:59 +0530

    header  animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { TokenService } from '../shared/token.service';
 import { AuthService } from '../shared/auth.service';
 import { AuthStateService } from '../shared/auth-state.service';
 import { ApiService } from '../shared/api.service';
-
+import { NgToastService } from 'ng-angular-popup';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit {
     private auth: AuthStateService,
     public router: Router,
     public token: TokenService,
-    public authApi: AuthService) { }
+    public authApi: AuthService, private toast:NgToastService) { }
 
   ngOnInit(): void {
   }
@@ -31,13 +31,15 @@ export class HeaderComponent implements OnInit {
 signOut(){
 this.authApi.logoutUser().subscribe ({
   next:(response)=>{
-    alert("Logged Out Succesfully");
+    this.toast.success({detail:"Success Message",summary:"Log Out Successflly  !!",duration:5000})
+    // alert("Logged Out Succesfully");
     this.auth.setAuthState(false);
     this.token.removeToken();
     this.router.navigate(['']);
   },
   error:(error)=>{
-    alert("Unauthorized")
+    this.toast.info({detail:"info Message",summary:"Something Wrong ,Session Time Out Login Again !!",duration:5000})
+
   }
 })
 }
