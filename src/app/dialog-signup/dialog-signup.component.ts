e5ec920a0baef14eb80e590401c2e4a513b2852f
Umme Salma <ummesalma2298@gmail.com>
2022-05-18 00:24:29 +0530

    dialog-signup animation

--- src/app/dialog-signup/dialog-signup.component.ts
+++ src/app/dialog-signup/dialog-signup.component.ts
@@ -2,6 +2,7 @@ import { Component,Inject,  OnInit } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgToastService } from 'ng-angular-popup';
 
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/auth.service';
@@ -20,10 +21,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./dialog-signup.component.css']
 })
 export class DialogSignupComponent implements OnInit {
-  // nameFormControl:any = new FormControl('', [Validators.required, Validators.required]);
-  // emailFormControl:any = new FormControl('', [Validators.required, Validators.email]);
-  // passwordFormControl:any = new FormControl('', [Validators.required, Validators.required, Validators.min(8)]);
-  // password_confirmationFormControl:any = new FormControl('', [Validators.required, Validators.required]);
+  hidePassword:boolean=true;
+  hideConfirmPassword:boolean= true;
+  nameFormControl:any = new FormControl('', [Validators.required, Validators.required]);
+  emailFormControl:any = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl:any = new FormControl('', [Validators.required, Validators.required, Validators.min(8)]);
+  password_confirmationFormControl:any = new FormControl('', [Validators.required, Validators.required]);
 
   actionBtn: string='Sign Up';
 
@@ -34,7 +37,9 @@ export class DialogSignupComponent implements OnInit {
     public router: Router,
     public fb: FormBuilder,
     public api: AuthService,
-    private dialogRef: MatDialogRef<DialogSignupComponent>)
+    private dialogRef: MatDialogRef<DialogSignupComponent>,
+    private toast : NgToastService
+    )
   {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
@@ -42,7 +47,14 @@ export class DialogSignupComponent implements OnInit {
       password: ['', Validators.required],
       password_confirmation: ['', Validators.required],
     });
+
   }
+
+ get f(){
+    return this.registerForm.controls;
+  }
+
+
   ngOnInit() {}
 
   onSubmit() {
@@ -50,13 +62,15 @@ export class DialogSignupComponent implements OnInit {
     this.api.register(this.registerForm.value).subscribe(
       (result) => {
         console.log(result);
-        alert('Registered Successfully');
+        // alert('Registered Successfully');
+        this.toast.success({detail:"Success Message",summary:"Resgiter Successfully Please Login!!",duration:5000})
         this.registerForm.reset();
         this.dialogRef.close("Sign Up");
         this.router.navigate(['']);
 
       },
       (error) => {
+        this.toast.info({detail:"info Message",summary:"Something Wrong !!, Try Again !!",duration:5000})
         this.errors = error.error;
       },
     );
