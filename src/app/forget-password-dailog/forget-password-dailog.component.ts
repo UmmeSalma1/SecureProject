e456345d0c283a6470fb2f5ef8db49be6a3b35a5
Umme Salma <ummesalma2298@gmail.com>
2022-05-18 00:29:24 +0530

    forget-password-dialog  animation

--- src/app/forget-password-dailog/forget-password-dailog.component.ts
+++ src/app/forget-password-dailog/forget-password-dailog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ForgetPasswordComponent } from '../forget-password/forget-password.component';
-
+import { NgToastService } from 'ng-angular-popup';
 @Component({
   selector: 'app-forget-password-dailog',
   templateUrl: './forget-password-dailog.component.html',
@@ -11,7 +11,7 @@ export class ForgetPasswordDailogComponent implements OnInit {
   // longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   // from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   // originally bred for hunting.`;
-  constructor(public router: Router) { }
+  constructor(public router: Router, private toast:NgToastService) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +19,6 @@ export class ForgetPasswordDailogComponent implements OnInit {
     this.router.navigate(['forgetPassword']);
   }
   resendlink(){
-
+    this.toast.success({detail:"Success Message",summary:" We Send a Mail Again Check your mail  !!",duration:5000})
   }
 }
